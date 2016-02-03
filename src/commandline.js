a0a359c25073500182b6d33b40b506f421ba604f
Cameron Hunter <chunter@twitter.com>
2016-02-02 23:54:13 -0800

    Pulling commandline out to separate, tested module

---
+++ src/commandline.js
@@ -0,0 +1,44 @@
+import nomnom from 'nomnom';
+import Path from 'path';
+import fs from 'fs';
+import Package from '../package.json';
+
+const exists = path => fs.accessSync(absolutePath(path));
+const absolutePath = path => Path.isAbsolute(path) ? path : Path.resolve(process.cwd(), path);
+
+const options = {
+  hostname: {
+    abbr: 'n',
+    default: 'localhost'
+  },
+  source: {
+    abbr: 's',
+    default: 9001
+  },
+  target: {
+    abbr: 't',
+    default: 9000
+  },
+  cert: {
+    abbr: 'c',
+    default: Path.resolve(__dirname, '..', 'resources', 'localhost.cert'),
+    callback: exists
+  },
+  key: {
+    abbr: 'k',
+    default: Path.resolve(__dirname, '..', 'resources', 'localhost.key'),
+    callback: exists
+  },
+  config: {
+    abbr: 'o',
+    callback: exists,
+    transform: path => require(absolutePath(path))
+  },
+  version: {
+    abbr: 'v',
+    flag: true,
+    callback: () => Package.version
+  }
+};
+
+export default nomnom.script(Package.name).options(options);
---
+++ test/commandline.js
@@ -0,0 +1,58 @@
+import test from 'ava';
+import fs from 'fs';
+import commandline from '../dist/commandline';
+
+test('cert (default)', t => {
+  const { cert } = commandline.parse([]);
+  t.ok(cert);
+});
+
+test('cert', t => {
+  const { cert } = commandline.parse(['--cert', '../resources/localhost.cert']);
+  t.ok(cert);
+});
+
+test('key (default)', t => {
+  const { key } = commandline.parse([]);
+  t.ok(key);
+});
+
+test('key', t => {
+  const { key } = commandline.parse(['--key', '../resources/localhost.key']);
+  t.ok(key);
+});
+
+test('hostname (default)', t => {
+  const { hostname } = commandline.parse([]);
+  t.is(hostname, 'localhost');
+});
+
+test('hostname', t => {
+  const { hostname } = commandline.parse(['--hostname', '127.0.0.1']);
+  t.is(hostname, '127.0.0.1');
+});
+
+test('source (default)', t => {
+  const { source } = commandline.parse([]);
+  t.is(source, 9001);
+});
+
+test('source', t => {
+  const { source } = commandline.parse(['--source', '5001']);
+  t.is(source, 5001);
+});
+
+test('target (default)', t => {
+  const { target } = commandline.parse([]);
+  t.is(target, 9000);
+});
+
+test('target', t => {
+  const { target } = commandline.parse(['--target', '5000']);
+  t.is(target, 5000);
+});
+
+test('config', t => {
+  const { config } = commandline.parse(['--config', './test-config.json']);
+  t.ok(config);
+});
